c751ee1c2ad33c07be6a6cd2f0afb5379fb20230
Nadjuska <nliljestrom@gmail.com>
2023-01-22 17:57:33 +0200

    First commit

---
+++ ServerProgrammingWeek1-2/src/main/java/spring23/ServerProgrammingWeek12/ServerProgrammingWeek12Application.java
@@ -0,0 +1,13 @@
+package spring23.ServerProgrammingWeek12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerProgrammingWeek12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerProgrammingWeek12Application.class, args);
+	}
+
+}
---
+++ ServerProgrammingWeek1-2/src/test/java/spring23/ServerProgrammingWeek12/ServerProgrammingWeek12ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring23.ServerProgrammingWeek12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerProgrammingWeek12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
