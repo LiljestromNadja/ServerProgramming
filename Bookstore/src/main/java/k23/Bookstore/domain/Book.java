893bc5a2db279f8d050b81e58b97455741f22c04
Nadjuska <nliljestrom@gmail.com>
2023-01-31 00:44:08 +0200

    teht 4 Bookstore

---
+++ Bookstore/src/main/java/k23/Bookstore/domain/Book.java
@@ -0,0 +1,10 @@
+package k23.Bookstore.domain;
+
+public class Book {
+	
+	private String title, author, isbn;
+	private int publicationYear, price;
+	
+	//En vielä tehnyt GET/SET, toString, Constructors
+
+}
